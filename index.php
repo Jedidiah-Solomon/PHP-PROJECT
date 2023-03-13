1428509373d4ac66e5dd8a6db0d784b57325871e
Jedidiah Solomon <jedybrown9821@gmail.com>
2023-03-13 22:12:24 +0100

    Added Variables

--- index.php
+++ index.php
@@ -10,6 +10,14 @@ $distance = 3500;
     <title>BitDegree Spaceship Project</title>
 	</head>
 	<body>
+    <ul>
+      <li>First list item</li>
+      <li>Second list item</li>
+      <li>Third list item</li>
+      <li>Fourth list item</li>
+      <li>Fifth list item</li>
+    </ul>
+
     <p>
       <h1>Project name: PHP Practice</h1>
       <?php
@@ -39,7 +47,59 @@ $distance = 3500;
         else {
           echo "<br>This is a short word!";
         }
+
+        //If Statement
+        $userName = "Amanda";
+        if($userName == "Amanda") {
+          echo "<br>I love you Amanda";
+        }
+        else {
+          echo "You are not my spec";
+        }
+
+        //Variables
+        $centralcomputer = "Works";
+
+        //The switch statement is used to replace if/else statements that 
+        //have multiple expressions with the same value. This command 
+        //is more efficient and easier to read.
+
       ?>
     </p>
+    <h4>Color recheck:</h4>
+    <p class="phptag">
+        <?php
+          // Switch code goes below
+          //Ans: The color is blue
+          switch ("blue") {
+            case "red":
+              echo 'The color is red';
+            break;
+            case "blue":
+              echo 'The color is blue';
+            break;
+            case "green":
+              echo 'The color is green';
+            break;
+            default:
+              echo "The color isn't red, blue or green";
+          }
+          //Second one
+          $color = "Pink";
+          switch ($color) {
+            case "red":
+              echo 'The color is red';
+            break;
+            case "blue":
+              echo 'The color is blue';
+            break;
+            case "green":
+              echo 'The color is green';
+            break;
+            default:
+              echo "The color isn't red, blue or green";
+          }
+        ?>
+      </p>
 	</body>
 </html>
